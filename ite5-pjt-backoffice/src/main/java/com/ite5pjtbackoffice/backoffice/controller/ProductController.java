7d15fc335476126b9f0c669b5dca0fb6a8e2c581
byeonyh95 <byeonyh95@gmail.com>
2021-12-03 21:15:23 +0900

    feat(UI): create html form

--- ite5-pjt-backoffice/src/main/java/com/ite5pjtbackoffice/backoffice/controller/ProductController.java
+++ ite5-pjt-backoffice/src/main/java/com/ite5pjtbackoffice/backoffice/controller/ProductController.java
@@ -1,7 +1,12 @@
 package com.ite5pjtbackoffice.backoffice.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ite5pjtbackoffice.backoffice.dto.Pager;
+import com.ite5pjtbackoffice.backoffice.vo.ProductColor;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -27,5 +32,14 @@ public class ProductController {
 	@RequestMapping("/display")
 	public String display() {
 		return "product/display";
+	}
+	
+	@RequestMapping("/productList")
+	public List<ProductColor> productList() {
+		
+		List<ProductColor> productList = null;
+		Pager pager = new Pager(0,0,0,0);
+		
+		return productList;
 	}
 }
