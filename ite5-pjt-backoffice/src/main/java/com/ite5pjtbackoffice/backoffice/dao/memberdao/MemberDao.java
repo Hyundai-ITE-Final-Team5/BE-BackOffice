2cc6e496ef08d80cf56797214fd9ed5b49c005c3
hyk339 <hyk339@gmail.com>
2021-12-08 09:35:25 +0900

    feat(Customer): 고객조건검색, 고객리스트, 고객정보수정

--- ite5-pjt-backoffice/src/main/java/com/ite5pjtbackoffice/backoffice/dao/memberdao/MemberDao.java
+++ ite5-pjt-backoffice/src/main/java/com/ite5pjtbackoffice/backoffice/dao/memberdao/MemberDao.java
@@ -1,8 +1,18 @@
 package com.ite5pjtbackoffice.backoffice.dao.memberdao;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
-@Mapper
-public class MemberDao {
+import com.ite5pjtbackoffice.backoffice.dto.Customer;
+import com.ite5pjtbackoffice.backoffice.dto.CustomerSearchOption;
 
+@Mapper
+public interface MemberDao {
+	public List<Customer> selectMemberList(Map<String, Object> map);
+	public int selectCount(CustomerSearchOption searchOption);
+	public Customer selectMemberByMid(String mid);
+	public int updateMember(Customer customer);
+	public int updateEnable(String mid);
 }
