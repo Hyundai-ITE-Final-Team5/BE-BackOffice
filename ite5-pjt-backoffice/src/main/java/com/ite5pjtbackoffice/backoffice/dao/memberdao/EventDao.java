d13da85f264556b89aeaff22714939f683bc7a85
hyk339 <hyk339@gmail.com>
2021-12-08 09:36:35 +0900

    feat(Promotion) : 이벤트리스트, 이벤트 등록 및 수정

---
+++ ite5-pjt-backoffice/src/main/java/com/ite5pjtbackoffice/backoffice/dao/memberdao/EventDao.java
@@ -0,0 +1,16 @@
+package com.ite5pjtbackoffice.backoffice.dao.memberdao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.ite5pjtbackoffice.backoffice.dto.EventSearchOption;
+import com.ite5pjtbackoffice.backoffice.vo.Event;
+
+@Mapper
+public interface EventDao {
+	public int selectCount(EventSearchOption eventSearchOption);
+	public List<Event> selectEventList(Map<String, Object> map);
+	public Event selectEvent(String eno);
+}
